e5e2037cfb76edd23a174e2bcaac8c42f478df0d
Hank <853160447@qq.com>
2020-11-20 15:50:14 +0800

    asm_amd64

---
+++ src/runtime/internal/sys/arch_amd64.go
@@ -0,0 +1,3 @@
+package sys
+
+type Uintreg uint64
--- src/runtime/runtime2.go
+++ src/runtime/runtime2.go
@@ -1,5 +1,10 @@
 package runtime
 
+import (
+	"runtime/internal/sys"
+	"unsafe"
+)
+
 type mutex struct{}
 
 type iface struct{
@@ -8,10 +13,99 @@ type iface struct{
 type eface struct{
 }
 
+// The guintptr, muintptr, and puintptr are all used to bypass write barriers.
+// It is particularly important to avoid write barriers when the current P has
+// been released, because the GC thinks the world is stopped, and an
+// unexpected write barrier would not be synchronized with the GC,
+// which can lead to a half-executed write barrier that has marked the object
+// but not queued it. If the GC skips the object and completes before the
+// queuing can occur, it will incorrectly free the object.
+//
+// We tried using special assignment functions invoked only when not
+// holding a running P, but then some updates to a particular memory
+// word went through write barriers and some did not. This breaks the
+// write barrier shadow checking mode, and it is also scary: better to have
+// a word that is completely ignored by the GC than to have one for which
+// only a few updates are ignored.
+//
+// Gs and Ps are always reachable via true pointers in the
+// allgs and allp lists or (during allocation before they reach those lists)
+// from stack variables.
+//
+// Ms are always reachable via true pointers either from allm or
+// freem. Unlike Gs and Ps we do free Ms, so it's important that
+// nothing ever hold an muintptr across a safe point.
+
+// A guintptr holds a goroutine pointer, but typed as a uintptr
+// to bypass write barriers. It is used in the Gobuf goroutine state
+// and in scheduling lists that are manipulated without a P.
+//
+// The Gobuf.g goroutine pointer is almost always updated by assembly code.
+// In one of the few places it is updated by Go code - func save - it must be
+// treated as a uintptr to avoid a write barrier being emitted at a bad time.
+// Instead of figuring out how to emit the write barriers missing in the
+// assembly manipulation, we change the type of the field to uintptr,
+// so that it does not require write barriers at all.
+//
+// Goroutine structs are published in the allg list and never freed.
+// That will keep the goroutine structs from being collected.
+// There is never a time that Gobuf.g's contain the only references
+// to a goroutine: the publishing of the goroutine in allg comes first.
+// Goroutine pointers are also kept in non-GC-visible places like TLS,
+// so I can't see them ever moving. If we did want to start moving data
+// in the GC, we'd need to allocate the goroutine structs from an
+// alternate arena. Using guintptr doesn't make that problem any worse.
+type guintptr uintptr
+
+type gobuf struct{
+	// The offsets of sp, pc, and g are known to (hard-coded in) libmach.
+	//
+	// ctxt is unusual with respect to GC: it may be a
+	// heap-allocated funcval, so GC needs to track it, but it
+	// needs to be set and cleared from assembly, where it's
+	// difficult to have write barriers. However, ctxt is really a
+	// saved, live register, and we only ever exchange it between
+	// the real register and the gobuf. Hence, we treat it as a
+	// root during stack scanning, which means assembly that saves
+	// and restores it doesn't need write barriers. It's still
+	// typed as a pointer so that any other writes from Go get
+	// write barriers.
+	sp uintptr
+	pc uintptr
+	g guintptr
+	ctxt unsafe.Pointer
+	ret sys.Uintreg
+	lr uintptr
+	bp uintptr
+}
+
 type sudog struct{
 }
 
-type g struct{}
+type stack struct{
+	lo uintptr
+	hi uintptr
+}
+
+type g struct{
+	// Stack parameters.
+	// stack describes the actual stack memory: [stack.lo, stack.hi).
+	// stackguard0 is the stack pointer compared in the Go stack growth prologue.
+	// It is stack.lo+StackGuard normally, but can be StackPreempt to trigger a preemption.
+	// stackguard1 is the stack pointer compared in the C stack growth prologue.
+	// It is stack.lo+StackGuard on g0 and gsignal stacks.
+	// It is ~0 on other goroutine stacks, to trigger a call to morestackc (and crash).
+	stack       stack   //执行栈 offset known to runtime/cgo
+	stackguard0 uintptr// offset known to liblink 在函数头部,编译器会插入一段指令，用它和SP寄存器进行比较，从而决定是否需要对栈空间扩容。另外,它还被用作抢占调度的标志
+	stackguard1 uintptr // offset known to liblink
+
+	m *m // current m; offset known to arm liblink
+}
+
+type m struct{
+	g0 *g
+	tls [6]uintptr // thread-local storage (for x86 extern register)
+}
 
 type itab struct {
 }
